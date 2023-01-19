83474a0387f359d0293f2616c4461422ff82ac15
oryx352 <122548249+oryx352@users.noreply.github.com>
2023-01-19 11:19:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,17 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("bye")
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showLeds(`
-        . # # # #
-        . . . . #
-        . . # # .
-        . . . . .
-        . . # . .
-        `)
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            . # # # #
+            . . . . #
+            . . # # .
+            . . . . .
+            . . # . .
+            `,10)
+basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
+    }
 })
 basic.showIcon(IconNames.Happy)
